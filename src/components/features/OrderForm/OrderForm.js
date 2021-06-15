a78df1e6ba978cf8b07ac18dd7ca9d9ab379be1b
Karolina Pastor <borek.karola@gmail.com>
2021-06-15 18:32:55 +0200

    Display all options

--- src/components/features/OrderForm/OrderForm.js
+++ src/components/features/OrderForm/OrderForm.js
@@ -2,9 +2,12 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import {Row, Col} from 'react-flexbox-grid';
 import OrderSummary from '../OrderSummary/OrderSummary';
+import pricing from '../../../data/pricing.json';
+import OrderOption from '../OrderOption/OrderOption';
 
 const OrderForm = props => (
   <Row>
+    {pricing.map(option => <Col md={4} key={option.id}><OrderOption name={option.name}>{console.log(option)}{option}</OrderOption></Col> )}
     <Col xs={12}>
       <OrderSummary cost={props.tripCost} options={props.options}/>
     </Col>
@@ -16,6 +19,7 @@ OrderForm.propTypes = {
   tripOptions: PropTypes.object,
   cost: PropTypes.string,
   options: PropTypes.object,
+  name: PropTypes.string,
 };
 
 export default OrderForm;
