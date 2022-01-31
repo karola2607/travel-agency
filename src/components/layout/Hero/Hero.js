0f0fb274fa81a06a44f83f0ff65517c099c82fd8
Karolina Pastor <borek.karola@gmail.com>
2022-01-31 17:33:02 +0100

    Install jest and enzyme, first test of component

--- src/components/layout/Hero/Hero.js
+++ src/components/layout/Hero/Hero.js
@@ -2,17 +2,26 @@ import React from 'react';
 import styles from './Hero.scss';
 import PropTypes from 'prop-types';
 
-const Hero = ({variant = '', titleText, imageSrc, ...otherProps}) => (
-  <div {...otherProps} className={styles.component + variant.split(' ').map(name => ' ' + (styles[name] || name)).join('')}>
-    <h2 className={styles.title}>{titleText}</h2>
-    <img className={styles.image}  src={imageSrc} />
-  </div>
+const Hero = ({ variant = '', titleText, imageSrc, ...otherProps }) => (
+    <div
+        {...otherProps}
+        className={
+            styles.component +
+            variant
+                .split(' ')
+                .map(name => ' ' + (styles[name] || name))
+                .join('')
+        }
+    >
+        <h2 className={styles.title}>{titleText}</h2>
+        <img className={styles.image} src={imageSrc} />
+    </div>
 );
 
 Hero.propTypes = {
-  variant: PropTypes.string,
-  titleText: PropTypes.node.isRequired,
-  imageSrc: PropTypes.string,
+    variant: PropTypes.string,
+    titleText: PropTypes.node.isRequired,
+    imageSrc: PropTypes.string.isRequired
 };
 
 export default Hero;
